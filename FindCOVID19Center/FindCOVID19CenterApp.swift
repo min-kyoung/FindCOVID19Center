e74e7be60951fd9eb5a786b85f15de85aefeb8bc
min-kyoung <shalsrud9@naver.com>
2022-04-01 20:13:10 +0900

    Initial Commit

---
+++ FindCOVID19Center/FindCOVID19CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCOVID19CenterApp.swift
+//  FindCOVID19Center
+//
+//  Created by 노민경 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct FindCOVID19CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
